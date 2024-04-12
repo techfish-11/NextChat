e41a1f7c610b0e704ce5c280ad281447b763a4cb
iruka541 <msuke1018@icloud.com>
2024-04-12 21:49:57 +0900

    beta

--- components/Chat.client.tsx
+++ components/Chat.client.tsx
@@ -1,24 +1,52 @@
-import { useEffect } from 'react';
+"use client";
+
+import React from 'react';
 import io from 'socket.io-client';
 
-const socket = io();
 
-const Chat = () => {
-  useEffect(() => {
-    // WebSocket connection
+const socket = io('http://localhost:4000');
+
+const HybridChatComponent = () => {
+  const [message, setMessage] = React.useState('');
+  const [messages, setMessages] = React.useState([]);
+
+  React.useEffect(() => {
+    // クライアントサイドでWebSocket接続
     socket.on('connect', () => {
-      console.log('Connected to WebSocket');
+      console.log('Connected to WebSocket on the client side');
     });
 
+    // メッセージを受信したときの処理
+    socket.on('message', (data: any) => {
+      setMessages((prevMessages: any[]) => [...prevMessages, data]);
+    });
+
+    // コンポーネントのアンマウント時にイベントリスナーを解除
     return () => {
-      socket.disconnect();
+      socket.off('connect');
+      socket.off('message');
     };
   }, []);
 
-  return null; // Rendering nothing
+  // メッセージを送信する関数
+  const sendMessage = () => {
+    if (message.trim()) {
+      socket.emit('message', message);
+      setMessage('');
+    }
+  };
+
+  return (
+    <div>
+      <input type="text" value={message} onChange={(e) => setMessage(e.target.value)} />
+      <button onClick={sendMessage}>送信</button>
+      <div>
+        {messages.map((msg, index) => (
+          <div key={index}>{msg}</div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
-// 親コンポーネントに"use client"を追加する
-Chat.useClient = true;
-
-export default Chat;
+export default HybridChatComponent;
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+app.use(cors()); // CORSミドルウェアを適用
+
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: "http://localhost:3000", // クライアントのオリジンを許可
+    methods: ["GET", "POST"] // 許可するHTTPメソッド
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+
+  socket.on('message', (message) => {
+    console.log('Message received: ' + message);
+    io.emit('message', message); // すべてのクライアントにメッセージを送信
+  });
+});
+
+const PORT = 4000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
